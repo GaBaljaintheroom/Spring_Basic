08b2d2ddfa800c7a164c3ccf1980267caf22778e
GaBaljaintheroom <junsu1222@naver.com>
2022-11-01 23:30:28 +0900

    Spring_Basic/AppConfig

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.discount.Order;
+import hello.core.discount.OrderService;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " +order);
+        System.out.println("oder.CalculatorPrice = " + order.CalculatePrice());
+    }
+}
